e1798f29542a94cf5cc4d19def1600b1aa52443a
Katrina Kalantar <kkalantar@berkeley.edu>
2018-11-01 16:31:50 -0700

    call test functions explictly

--- tests/testthat/test-injectlabelnoise.R
+++ tests/testthat/test-injectlabelnoise.R
@@ -5,13 +5,13 @@ initial_classes <- c(rep(1,100), rep(2,100))
 initial_fd <- rep(-1, length(initial_classes))
 
 test_that("flipping all lables works", {
-  noised_classes <- inject_label_noiseR(initial_classes, 1, 1)
+  noised_classes <- metahostlab::inject_label_noiseR(initial_classes, 1, 1)
   expect_equal(noised_classes$yz, rev(initial_classes))
   expect_equal(noised_classes$fd, initial_fd*-1)
 })
 
 test_that("flipping only one class works", {
-  noised_classes <- inject_label_noiseR(initial_classes, 0, 1)
+  noised_classes <- metahostlab::inject_label_noiseR(initial_classes, 0, 1)
   expect_equal(noised_classes$yz, rep(1, length(initial_classes)))
   expect_equal(noised_classes$fd, c(rep(-1, length(initial_classes)/2),rep(1, length(initial_classes)/2)))
 })
