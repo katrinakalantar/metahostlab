1592761042118d68395b275f7f786c4b0faebcfa
Katrina Kalantar <kkalantar@berkeley.edu>
2018-11-01 17:10:28 -0700

    revert test cases and update travis yml attempt to fix build fail

--- tests/testthat/test-castlabel.R
+++ tests/testthat/test-castlabel.R
@@ -1,8 +1,8 @@
 context("test cast label")
 
 test_that("casting label works", {
-  expect_equal(metahostlab::castLabel(c(1,1,2,2), 2), c(1,1,2,2))
-  expect_equal(metahostlab::castLabel(c(1,1,2,2), -1), c(-1,-1,1,1))
-  expect_equal(metahostlab::castLabel(c(2,2,1,1), -1), c(1,1,-1,-1))
-  expect_equal(metahostlab::castLabel(c(2,2,1,1), 2), c(2,2,1,1))
+  expect_equal(castLabel(c(1,1,2,2), 2), c(1,1,2,2))
+  expect_equal(castLabel(c(1,1,2,2), -1), c(-1,-1,1,1))
+  expect_equal(castLabel(c(2,2,1,1), -1), c(1,1,-1,-1))
+  expect_equal(castLabel(c(2,2,1,1), 2), c(2,2,1,1))
 })
